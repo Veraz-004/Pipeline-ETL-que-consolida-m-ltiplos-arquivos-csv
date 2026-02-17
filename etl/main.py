6db457784d809fd5bf4bdf27cda4f57111e5d3b0
Veras <felipeverads2@gmail.com>
2026-02-17 17:13:35 -0300

    pequeno ajuste

--- etl/main.py
+++ etl/main.py
@@ -4,11 +4,20 @@ from load import Load
 from analysis import lucro_max, receita_total, dia_quantidade, dia_receita
 
 #* este arquivo só irá rodar as funções dos outros arquivos
-if __name__ == "__main__":
+def rodar_pipeline():
     silver, gold, arquivos = Extract()
     arqvs, arqvs_gold = Transform(arquivos)
     Load(arqvs, arqvs_gold, silver, gold)
-    lucro_max(gold)
-    receita_total(gold)
-    dia_quantidade(gold)
-    dia_receita(gold)
+    return arqvs, gold
+
+def rodar_analysis(arqvs):
+    if arqvs != []:
+        lucro_max(gold)
+        receita_total(gold)
+        dia_quantidade(gold)
+        dia_receita(gold)
+
+
+if __name__ == "__main__":
+    arqvs, gold=rodar_pipeline()
+    rodar_analysis(arqvs)
